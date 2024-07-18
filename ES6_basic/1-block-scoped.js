852aa435174c6e11752981e419052aff4ed2ebbb
Inès Oubabas <143883134+Ines-Oubabas@users.noreply.github.com>
2024-07-18 16:18:56 +0200

    Update 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,9 +3,11 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
+    (function() {
     var task = true;
     var task2 = false;
+  })();
   }
-
+  
   return [task, task2];
 }
