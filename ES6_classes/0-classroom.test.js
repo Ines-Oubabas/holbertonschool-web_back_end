2cd2d2e91088f6897fa57c4c6028503866ada08d
Ines-Oubabas <inesoubabas56@gmail.com>
2024-07-19 17:08:53 +0200

    delete

--- ES6_classes/0-classroom.test.js
+++
@@ -1,8 +0,0 @@
-import ClassRoom from './0-classroom';
-
-describe('ClassRoom', () => {
-  it('should set maxStudentsSize properly', () => {
-    const room = new ClassRoom(10);
-    expect(room._maxStudentsSize).toBe(10);
-  });
-});
