ab377695edd29919d956606d6fea2777c2cf4842
Ines-Oubabas <inesoubabas56@gmail.com>
2024-09-20 19:49:45 +0200

    task4 update

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,16 +1,13 @@
-const { createServer } = require('http');
+const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
-
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!\n');
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(1245, () => {
+    console.log('Server listening on port 1245');
 });
 
 module.exports = app;
