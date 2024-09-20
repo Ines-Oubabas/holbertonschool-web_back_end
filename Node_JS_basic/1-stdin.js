ab8fff11ca62d2154e260fa8fc1c361fc1c7b2b0
Ines-Oubabas <inesoubabas56@gmail.com>
2024-09-20 13:30:17 +0200

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');
+  console.log(`Your name is: ${name}`);
   process.exit();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
